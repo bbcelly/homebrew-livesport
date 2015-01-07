f8e6a8a6abf2608415a4b5674120df8b05e4bbd6
belakos <belakos@livesport.eu>
2015-01-07 09:19:22 +0100

    lsgitflow_1.5.6-2+ubuntu_all

--- lsgitflow.rb
+++ lsgitflow.rb
@@ -7,18 +7,18 @@ require "formula"
 
 class Lsgitflow < Formula
   package = "lsgitflow"
-  version = "1.5.6-1+ubuntu_all"
+  version = "1.5.6-2+ubuntu_all"
   urlPrefix = "http://#{package}.belakos/"
 
   version version
 
   url "#{urlPrefix}#{version}/#{package}_#{version}.tar.gz"
-  sha1 "49f0d23225d480ebecdd5e342109ba68f52d314c"
+  sha1 "b8b8726c728d089e68e11cd0c3eebcc3f8a956a5"
   depends_on "lsdevtools"
 
   patch :p2 do
     url "#{urlPrefix}#{version}/#{package}_#{version}.diff"
-    sha1 "14a69378fc93d10a7c6f8e6db9f9c753012b4d88"
+    sha1 "4372060727d582992db9f53e38c340d521ac9ec0"
   end
 
   def install
