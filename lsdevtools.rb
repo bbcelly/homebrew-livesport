1cfd9b21b7c6a03f5b86342aaa57920e7c0c3388
belakos <belakos@livesport.eu>
2015-09-14 13:36:02 +0200

    lsdevtools_1.15.29-1-a+trusty_all

--- lsdevtools.rb
+++ lsdevtools.rb
@@ -7,7 +7,7 @@ require "formula"
 
 class Lsdevtools < Formula
   package = "lsdevtools"
-  version = "1.15.29-1+trusty_all-aaa"
+  version = "1.15.29-1-a+trusty_all"
   urlPrefix = "http://#{package}.belakos/"
 
   version version
